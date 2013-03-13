e666647a0f4c395d5a947169cf19fbd215484bc6
Marius Kristensen <marius.kristensen@gmail.com>
2013-03-13 19:22:32 +0100

    Add KeyListeners to TetrisPanel and add rectangular shape to it

--- src/main/java/com/cozycoding/crea1/Tetris.java
+++ src/main/java/com/cozycoding/crea1/Tetris.java
@@ -1,20 +1,21 @@
 package com.cozycoding.crea1;
 
+import com.cozycoding.crea1.shapes.RectangleBlock;
+
 import java.awt.*;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
+public class Tetris {
     private static final int windowWidth = 320;
     private static final int windowHeight = 240;
     private static final String title = "#1 Window - Cozy Coding";
 
     public static void main(String[] args) {
         Window window = new Window(title, new Dimension(windowWidth, windowHeight));
-        final TetrisPanel tetrisPanel = new TetrisPanel(new Dimension(windowWidth, windowHeight));
-        window.getContentContainer().add(tetrisPanel);
+        RectangleBlock rectangleBlock = new RectangleBlock();
+        final TetrisPanel tetrisPanel = new TetrisPanel(new Dimension(windowWidth, windowHeight), rectangleBlock);
+        window.add(tetrisPanel);
     }
 }
--- src/main/java/com/cozycoding/crea1/TetrisPanel.java
+++ src/main/java/com/cozycoding/crea1/TetrisPanel.java
@@ -1,38 +1,77 @@
 package com.cozycoding.crea1;
 
+import com.cozycoding.crea1.shapes.TetrisShape;
+
 import javax.swing.*;
 import java.awt.*;
-import java.awt.geom.RoundRectangle2D;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 /**
- * Draws the "world".. that is, a
+ * Panel where all the Tetris action happens
  *
  * @author Marius Kristensen
  */
 public class TetrisPanel extends JPanel {
+
+
     private static final Color skyColor = new Color(0x87CEEB);
     private Double width;
     private Double height;
-    public TetrisPanel(Dimension dimension) {
+    private TetrisShape activeTetrisShape;
+
+    public TetrisPanel(Dimension dimension, TetrisShape tetrisShape) {
         setPreferredSize(dimension);
         this.width = dimension.getWidth();
         this.height = dimension.getHeight();
+        this.activeTetrisShape = tetrisShape;
+        addKeyListener(new TAdapter());
+        setFocusable(true);
+        setVisible(true);
     }
 
     @Override
     public void paintComponent(Graphics g) {
         clear(g);
         Graphics2D graphics2D = (Graphics2D) g;
-        drawSky(graphics2D);
-    }
-
-    private void drawSky(Graphics2D g) {
-        g.setColor(skyColor);
-        g.fill(new RoundRectangle2D.Double(0, 0, width, height, 0, 0));
+        graphics2D.setColor(activeTetrisShape.getColor());
+        graphics2D.fill(activeTetrisShape.getShape());
     }
 
 
     private void clear(Graphics g) {
         super.paintComponent(g);
     }
+
+    /**
+     * Keylisteners
+     */
+    private class TAdapter extends KeyAdapter {
+
+        @Override
+        public void keyReleased(KeyEvent e) {
+            //
+        }
+
+        @Override
+        public void keyPressed(KeyEvent keyEvent) {
+
+            switch (keyEvent.getKeyCode()) {
+                case 37:
+                    activeTetrisShape.onLeftKeyPressedEvent();
+                    break;
+                case 38:
+                    activeTetrisShape.onUpKeyPressedEvent();
+                    break;
+                case 39:
+                    activeTetrisShape.onRightKeyPressedEvent();
+                    break;
+                case 40:
+                    activeTetrisShape.onDownKeyPressedEvent();
+                    break;
+            }
+            repaint();
+        }
+    }
+
 }
