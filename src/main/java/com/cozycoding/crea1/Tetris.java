99542f81d55cccfd64b149b5fa7ad5408fd9a6f0
Marius Kristensen <marius.kristensen@gmail.com>
2013-03-13 19:50:30 +0100

    Change window title

--- src/main/java/com/cozycoding/crea1/Tetris.java
+++ src/main/java/com/cozycoding/crea1/Tetris.java
@@ -5,12 +5,15 @@ import com.cozycoding.crea1.shapes.RectangleBlock;
 import java.awt.*;
 
 /**
- * Hello world!
+ * Tetris
+ * Well, kind of, and not yet. Still a long way to go...
+ *
+ * @author Marius Haugli Kristensen
  */
 public class Tetris {
     private static final int windowWidth = 320;
     private static final int windowHeight = 240;
-    private static final String title = "#1 Window - Cozy Coding";
+    private static final String title = "Tetris by crea1 - Cozy Coding";
 
     public static void main(String[] args) {
         Window window = new Window(title, new Dimension(windowWidth, windowHeight));
