ef042c9dbb47350ecb2d62b30edde914387c7014
Marius Kristensen <marius.kristensen@gmail.com>
2013-03-13 22:39:24 +0100

    Create isAtBottom to figure out when shape is at bottom

--- src/main/java/com/cozycoding/crea1/TetrisPanel.java
+++ src/main/java/com/cozycoding/crea1/TetrisPanel.java
@@ -17,10 +17,14 @@ import java.awt.event.KeyEvent;
  */
 public class TetrisPanel extends JPanel {
 
+    private final Double width;
+    private final Double height;
     private TetrisShape activeTetrisShape;
 
     public TetrisPanel(Dimension dimension, TetrisShape tetrisShape) {
         setPreferredSize(dimension);
+        this.width = dimension.getWidth();
+        this.height = dimension.getHeight();
         this.activeTetrisShape = tetrisShape;
         addKeyListener(new TAdapter());
         setFocusable(true);
@@ -32,10 +36,11 @@ public class TetrisPanel extends JPanel {
         clear(g);
         Graphics2D graphics2D = (Graphics2D) g;
         Double width = activeTetrisShape.getWidth();
-        double posX = activeTetrisShape.getX();
-        double posY = activeTetrisShape.getY();
+        double x = activeTetrisShape.getX();
+        double y = activeTetrisShape.getY();
         Double height = activeTetrisShape.getHeight();
-        graphics2D.rotate(Math.toRadians(activeTetrisShape.getRotation()), posX + width/2, posY + height/2);
+        //todo find another way to rotate
+        graphics2D.rotate(Math.toRadians(activeTetrisShape.getRotation()), x + width / 2, y + height / 2);
         graphics2D.setColor(activeTetrisShape.getColor());
         graphics2D.fill(activeTetrisShape.getShape());
     }
@@ -57,23 +62,32 @@ public class TetrisPanel extends JPanel {
 
         @Override
         public void keyPressed(KeyEvent keyEvent) {
-
-            switch (keyEvent.getKeyCode()) {
-                case 37:
-                    activeTetrisShape.setX(activeTetrisShape.getX() - 20);
-                    break;
-                case 38:
-                    activeTetrisShape.setRotation(activeTetrisShape.getRotation() + 90);
-                    break;
-                case 39:
-                    activeTetrisShape.setX(activeTetrisShape.getX() + 20);
-                    break;
-                case 40:
-                    activeTetrisShape.setY(activeTetrisShape.getY() + 20);
-                    break;
+            if (!isAtBottom()) {
+                switch (keyEvent.getKeyCode()) {
+                    case 37:
+                        activeTetrisShape.setX(activeTetrisShape.getX() - 20);
+                        break;
+                    case 38:
+                        activeTetrisShape.setRotation(activeTetrisShape.getRotation() + 90);
+                        break;
+                    case 39:
+                        activeTetrisShape.setX(activeTetrisShape.getX() + 20);
+                        break;
+                    case 40:
+                        activeTetrisShape.setY(activeTetrisShape.getY() + 20);
+                        break;
+                }
             }
+
+            System.out.println("keyEvent = " + isAtBottom());
             repaint();
         }
+    }
+
+    private boolean isAtBottom() {
+        double v = activeTetrisShape.getY() + activeTetrisShape.getHeight();
+        return (v == height);
+
     }
 
 }
