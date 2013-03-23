5bba3359b79d0a4e202d1516ab137b46f357ab8b
Marius Kristensen <marius.kristensen@gmail.com>
2013-03-23 23:44:29 +0100

    Add JFrame window

---
+++ src/main/java/com/cozycoding/crea1/newtry/Window.java
@@ -0,0 +1,21 @@
+package com.cozycoding.crea1.newtry;
+
+import javax.swing.JFrame;
+import java.awt.Dimension;
+import java.awt.HeadlessException;
+
+/**
+ * @author Marius Kristensen
+ */
+public class Window extends JFrame {
+
+    public Window(String title, Dimension dimension) throws HeadlessException {
+        setTitle(title);
+        setSize(dimension);
+        setResizable(false);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+    }
+
+}
