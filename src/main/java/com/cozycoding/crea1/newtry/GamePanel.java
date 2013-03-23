8225a7ba751cb80f333447c5509121c78c6bc0dd
Marius Kristensen <marius.kristensen@gmail.com>
2013-03-23 23:46:04 +0100

    Add simple gui with keylisteners

---
+++ src/main/java/com/cozycoding/crea1/newtry/GamePanel.java
@@ -0,0 +1,77 @@
+package com.cozycoding.crea1.newtry;
+
+import com.cozycoding.crea1.newtry.Blocks.Cell;
+import com.cozycoding.crea1.newtry.Blocks.TetrisBlock;
+import com.cozycoding.crea1.shapes.TetrisShape;
+
+import javax.swing.JPanel;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.RoundRectangle2D;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Marius Kristensen
+ */
+public class GamePanel extends JPanel {
+
+    private final Double height;
+    private final Double width;
+    private final Double cellSize;
+    private List<RoundRectangle2D> cells = new ArrayList<>();
+    private List<RoundRectangle2D> board = new ArrayList<>();
+    private static final Color color = new Color(0xFF0056);
+    private static final Color emptyCells = new Color(0x3377FF);
+
+    public GamePanel(Dimension dimension) {
+        setPreferredSize(dimension);
+        this.width = dimension.getWidth();
+        this.height = dimension.getHeight();
+        this.cellSize = dimension.getWidth() / 10;
+        setFocusable(true);
+        setVisible(true);
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        clear(g);
+        Graphics2D graphics2D = (Graphics2D) g;
+
+        for (RoundRectangle2D cell : board) {
+            graphics2D.setColor(emptyCells);
+            graphics2D.fill(cell);
+        }
+
+        for (RoundRectangle2D cell : cells) {
+            graphics2D.setColor(color);
+            graphics2D.fill(cell);
+        }
+    }
+
+    public void paintGameBoard(Cell[][] gameBoard, TetrisBlock activeBlock) {
+        board.removeAll(board);
+        cells.removeAll(cells);
+        for (int i = 0; i < gameBoard.length; i++) {
+            for (int j = 0; j < gameBoard[i].length; j++) {
+                Cell cell = gameBoard[i][j];
+                board.add(createRectangle(cell.getX(), cell.getY()));
+            }
+        }
+        for (Cell cell : activeBlock.getShape()) {
+            cells.add(createRectangle(cell.getX(), cell.getY()));
+            System.out.println(cell.getX() + " " + cell.getY());
+        }
+        repaint();
+    }
+
+    private void clear(Graphics g) {
+        super.paintComponent(g);
+    }
+
+    private RoundRectangle2D.Double createRectangle(double x, double y) {
+        return new RoundRectangle2D.Double(x * cellSize, y * cellSize, cellSize, cellSize, 0, 0);
+    }
+}
--- src/main/java/com/cozycoding/crea1/newtry/Tetris2.java
+++ src/main/java/com/cozycoding/crea1/newtry/Tetris2.java
@@ -1,16 +1,57 @@
 package com.cozycoding.crea1.newtry;
 
+import com.cozycoding.crea1.newtry.Blocks.SquareBlock;
+
+import java.awt.Dimension;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
 /**
  * @author Marius Kristensen
  */
 public class Tetris2 {
-    private GameStatus gameStatus = GameStatus.NOT_STARTED;
+    private static final int windowWidth = 320;
+    private static final int windowHeight = 576;
+
+    private static final String title = "Tetris by crea1 - Cozy Coding";
+    private static GameRules gameRules;
+    private static GamePanel gamePanel;
+
     public static void main(String[] args) {
+        gameRules = new GameRules();
+        gameRules.placeBlockOnGameBoard(new SquareBlock());
+        Window window = new Window(title, new Dimension(windowWidth, windowHeight));
+        gamePanel = new GamePanel(new Dimension(windowWidth, windowHeight));
+        gamePanel.addKeyListener(new TAdapter());
+        gamePanel.paintGameBoard(gameRules.getGameBoard(), gameRules.getActiveBlock());
+        window.add(gamePanel);
 
     }
+    private static class TAdapter extends KeyAdapter {
 
+        @Override
+        public void keyReleased(KeyEvent e) {
+            //
+        }
 
-    public GameStatus getGameStatus() {
-        return gameStatus;
+        @Override
+        public void keyPressed(KeyEvent keyEvent) {
+            switch (keyEvent.getKeyCode()) {
+                case 37:
+                    gameRules.moveActiveBlockLeft();
+                    break;
+                case 38:
+                    //TODO Rotate block
+                    break;
+                case 39:
+                    gameRules.moveActiveBlockRight();
+                    break;
+                case 40:
+                    gameRules.moveActiveBlockDown();
+                    break;
+            }
+            gamePanel.paintGameBoard(gameRules.getGameBoard(), gameRules.getActiveBlock());
+        }
     }
+
 }
