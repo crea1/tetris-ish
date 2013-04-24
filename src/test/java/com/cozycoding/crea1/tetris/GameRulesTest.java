4d20bbf1539c62438c2ad5c81aef628ac19ba902
Marius Kristensen <marius.kristensen@gmail.com>
2013-04-24 23:47:52 +0200

    Replace deprecated junit imports

--- src/test/java/com/cozycoding/crea1/tetris/GameRulesTest.java
+++ src/test/java/com/cozycoding/crea1/tetris/GameRulesTest.java
@@ -5,10 +5,10 @@ import com.cozycoding.crea1.tetris.blocks.OBlock;
 import org.junit.Before;
 import org.junit.Test;
 
-import static junit.framework.Assert.assertEquals;
-import static junit.framework.Assert.assertFalse;
-import static junit.framework.Assert.assertTrue;
-import static junit.framework.Assert.fail;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 /**
  * @author Marius Kristensen
